e567c4f9ab20c6211a54e95a8a7b1c0d5bcd552b
Ferrer Benoit <ferrer-benoit@MacBook-Pro-de-Ferrer.local>
2018-11-08 16:38:30 +0100

    Only one container

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,39 @@
 import { Component } from "react";
 import styles from './stylesheets/index.scss' 
-import EmojisComponent from './emojis/component/emojis-component'
+import  FloatingEmojisContainer  from './emojis/container/floating-emojis-container'
+
 
 class Index extends Component {
-  render() {
+  constructor() {
+    super(...arguments)
+    this.state = {
+        emojis: {
+            numberOfEmojis: 0
+        }
+    }
+}
+
+sendEmoji = () => {
+    this.setState({
+        emojis: {
+            numberOfEmojis: 1, // Number emojis send
+            img: 'cat.png' // image
+        }
+    })
+}
+
+
+render() {
+    const emoji = {
+        numberOfEmojis: 4
+    }
     return (
-      <div className='container'>
-        <EmojisComponent />
-      </div>
+        <div className='content-emoji'> 
+            <div className='input-emoji' onClick={this.sendEmoji}>Click here</div>
+            <FloatingEmojisContainer emojis={this.state.emojis} />
+        </div>
     )
-  }
+}
 }
 
 export default Index
